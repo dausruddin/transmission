fd8fbfe387ea504b1134c4167f929ce5fe64e85e
dausruddin <5763466+dausruddin@users.noreply.github.com>
2019-10-17 03:38:20 +0800

    Revert "Slashes fixes"

--- libtransmission/rpc-server.c
+++ libtransmission/rpc-server.c
@@ -685,22 +685,18 @@ static void handle_request(struct evhttp_request* req, void* arg)
 
         server->loginattempts = 0;
 
-        size_t const server_url_len = strlen(server->url);
-        char const* const location = strncmp(req->uri, server->url, server_url_len) == 0 ? req->uri + server_url_len : NULL;
-
-        if (location == NULL || location[0] == '\0' ||
-            strcmp(location, "web") == 0)
+        if (strncmp(req->uri, server->url, strlen(server->url)) != 0)
         {
-            char* new_location = tr_strdup_printf("%sweb/", server->url);
-            evhttp_add_header(req->output_headers, "Location", new_location);
+            char* location = tr_strdup_printf("%sweb/", server->url);
+            evhttp_add_header(req->output_headers, "Location", location);
             send_simple_response(req, HTTP_MOVEPERM, NULL);
-            tr_free(new_location);
+            tr_free(location);
         }
-        else if (strncmp(location, "web/", 4) == 0)
+        else if (strncmp(req->uri + strlen(server->url), "web/", 4) == 0)
         {
             handle_web_client(req, server);
         }
-        else if (strcmp(location, "upload") == 0)
+        else if (strcmp(req->uri + strlen(server->url), "upload") == 0)
         {
             handle_upload(req, server);
         }
@@ -745,7 +741,7 @@ static void handle_request(struct evhttp_request* req, void* arg)
 
 #endif
 
-        else if (strncmp(location, "rpc", 3) == 0)
+        else if (strncmp(req->uri + strlen(server->url), "rpc", 3) == 0)
         {
             handle_rpc(req, server);
         }
@@ -1135,22 +1131,14 @@ tr_rpc_server* tr_rpcInit(tr_session* session, tr_variant* settings)
     }
 
     key = TR_KEY_rpc_url;
-    size_t url_len;
 
-    if (!tr_variantDictFindStr(settings, key, &str, &url_len))
+    if (!tr_variantDictFindStr(settings, key, &str, NULL))
     {
         missing_settings_key(key);
     }
     else
     {
-        if (url_len == 0 || str[url_len - 1] != '/')
-        {
-            s->url = tr_strdup_printf("%s/", str);
-        }
-        else
-        {
-            s->url = tr_strdup(str);
-        }
+        s->url = tr_strdup(str);
     }
 
     key = TR_KEY_rpc_whitelist_enabled;
